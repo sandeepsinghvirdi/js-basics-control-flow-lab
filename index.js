5fa1e1e485173089992a1751caf893f6ee0dfc27
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-06-02 12:47:57 +1000

    commit tree c1f5c0b8c3623f105246eaddc33eddd201906380

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+let someNumber
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+
+  if (someNumber <= 400) {
+    return "This one is on me!";
+  }
+  if (someNumber > 2000 && someNumber <= 2500) {
+  return "I will gladly take your thirty bucks.";
+  }
+ else
+    return "No can do.";
+  }
+
+let cityName
+function ternaryCheckCity(cityName){
+  if (cityName == 'NYC'){
+    return "Ok, sounds good."
+  }
+  if (cityName == 'Pittsburgh'){
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
   // Write your code here!
+
+  let feedback 
+function switchOnCharmFromTip(feedback){
+  if (feedback == 'generous'){
+  return "Thank you so much.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+if (feedback =='not as generous'){
+  return "Thank you.";
 }
+if (feedback == 'thanks for everything'){
+return 'Bye.';
+}
+}
+
+  // Write your code here!
